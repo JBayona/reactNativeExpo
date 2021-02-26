e0730900c10c3063cf35abfb5acdfcd7a2390b58
JBayona <jordavids_22@hotmail.com>
2021-02-25 23:41:41 -0600

    Create component for table.

--- HelloUser.js
+++ HelloUser.js
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
-import Parse from 'parse/react-native';
-import { DataTable } from 'react-native-paper';
-import Styles from './Styles';
+import React, { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import Parse from "parse/react-native";
+import { DataTable } from "react-native-paper";
+import Styles from "./Styles";
 
 // Component
-import {AttendeeTable} from './components/table';
+import AttendeeTable from "./components/table";
 
 export const HelloUser = () => {
   // State variable that will hold username value
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
   // useEffect is called after the component is initially rendered and
   // after every other render
@@ -19,7 +19,7 @@ export const HelloUser = () => {
     // function here and call it afterwards
     async function getCurrentUser() {
       // This condition ensures that username is updated only if needed
-      if (username === '') {
+      if (username === "") {
         const currentUser = await Parse.User.currentAsync();
         if (currentUser !== null) {
           setUsername(currentUser.getUsername());
@@ -34,41 +34,10 @@ export const HelloUser = () => {
   return (
     <View style={Styles.login_wrapper}>
       <View style={Styles.form}>
-        {username !== '' && <Text>{`Hello ${username}!`}</Text>}
-        <Text>{'This is the home Screen where we can see the details'}</Text>
-        <AttendeeTable/>
+        {username !== "" && <Text>{`Hello ${username}!`}</Text>}
+        <Text>{"This is the home Screen where we can see the details"}</Text>
+        <AttendeeTable />
       </View>
     </View>
   );
 };
-
-const MyComponent = () => (
-  <DataTable>
-    <DataTable.Header>
-      <DataTable.Title>Dessert</DataTable.Title>
-      <DataTable.Title numeric>Calories</DataTable.Title>
-      <DataTable.Title numeric>Fat</DataTable.Title>
-    </DataTable.Header>
-
-    <DataTable.Row>
-      <DataTable.Cell>Frozen yogurt</DataTable.Cell>
-      <DataTable.Cell numeric>159</DataTable.Cell>
-      <DataTable.Cell numeric>6.0</DataTable.Cell>
-    </DataTable.Row>
-
-    <DataTable.Row>
-      <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-      <DataTable.Cell numeric>237</DataTable.Cell>
-      <DataTable.Cell numeric>8.0</DataTable.Cell>
-    </DataTable.Row>
-
-    <DataTable.Pagination
-      page={1}
-      numberOfPages={3}
-      onPageChange={page => {
-        console.log(page);
-      }}
-      label="1-2 of 6"
-    />
-  </DataTable>
-);
